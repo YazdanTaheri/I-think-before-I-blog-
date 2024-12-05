dbbdd46d3642ee13cc9c89e3d3bf1c402209ad27
Yazdan-Cristian (Dan) <danitaheri1@gmail.com>
2024-12-05 15:33:55 +0000

    updated code

--- codestar/settings.py
+++ codestar/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_aztdw9i(e%oy^hbepo&6&-)u33593$iqmdk$%7j3_q&(zubf8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOPMENT')
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-yazdantaher-ithinkbefor-refex4zo0r2.ws.codeinstitute-ide.net']
 
@@ -76,12 +80,21 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+  #    #     'NAME': BASE_DIR / 'db.sqlite3',
+#}
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
