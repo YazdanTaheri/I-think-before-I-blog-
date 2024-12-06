ef4644bf449c394ce3fac536594cdf97b7c0f7e9
Yazdan-Cristian (Dan) <danitaheri1@gmail.com>
2024-12-06 14:57:48 +0000

    Add CSRF_TRUSTED_ORIGINS to settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,6 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_aztdw9i(e%oy^hbepo&6&-)u33593$iqmdk$%7j3_q&(zubf8'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
