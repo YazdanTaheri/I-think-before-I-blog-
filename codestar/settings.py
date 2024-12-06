22ba47683ad823b3b5c2f713e3b5eaf06c712281
Yazdan-Cristian (Dan) <danitaheri1@gmail.com>
2024-12-06 10:52:14 +0000

    Fix for Heroku error

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_aztdw9i(e%oy^hbepo&6&-)u33593$iqmdk$%7j3_q&(zubf8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-yazdantaher-ithinkbefor-refex4zo0r2.ws.codeinstitute-ide.net',
+ALLOWED_HOSTS = ['8000-yazdantaher-ithinkbefor-refex4zo0r2.ws.codeinstitute-ide.net/',
 '.herokuapp.com']
 
 
