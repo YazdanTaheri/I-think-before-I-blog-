0ff4602ac24232cd03a8b25c8f340db9def69f3b
Yazdan-Cristian (Dan) <danitaheri1@gmail.com>
2024-12-09 18:13:51 +0000

    break time now\safety safe

--- blog/admin.py
+++ blog/admin.py
@@ -6,10 +6,10 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status')
+    list_display = ('title', 'slug', 'status','created_on')
     search_fields = ['title']
-    list_filter = ('status',)
-    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status','created_on',)
+    prepopulated_fields = {'slug': ('title','content')}
     summernote_fields = ('content',)
 
 
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from . import views
-from django.urls import path
+from django.urls import path, include  # Ensure include is imported
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('summernote/', include('django_summernote.urls')),
 ]
